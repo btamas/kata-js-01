08c8ab4f31d10bba24db2521d7d4832fd1ca5930
btamas <tamas@besenyei.net>
2014-07-25 16:51:02 +0200

    some refactor

--- libs/permutations.js
+++ libs/permutations.js
@@ -5,17 +5,19 @@ Permutation.prototype.getPermutations = function getPermutations( text ) {
 };
 
 Permutation.prototype._getPermutations = function _getPermutations( prefix, text ) {
-	var n = text.length;
-	if ( n === 0 ) {
+	if ( text.length === 0 ) {
 		return [ prefix ];
 	}
 	else {
 		var result = [];
-		for( var i=0; i < n; i++ ) {
-			if ( text.indexOf( text[i] ) >= i ) {
-				var p = this._getPermutations( prefix + text[i], text.substr(0, i) + text.substr(i+1) );
-				result = result.concat(p);
+		for( var i=0; i < text.length; i++ ) {
+			//prevent duplications
+			if ( text.indexOf( text[i] ) < i ) {
+				continue;
 			}
+
+			var permutations = this._getPermutations( prefix + text[i], text.substr(0, i) + text.substr(i+1) );
+			result = result.concat(permutations);
 		}
 		return result;
 	}
